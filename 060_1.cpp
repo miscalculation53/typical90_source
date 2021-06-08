c2bf4a1444e886b75ea5059a3fcad9a2487fb2f3
miscalculation53 <50172036+miscalculation53@users.noreply.github.com>
2021-06-08 17:38:02 +0900

    Update 060_1.cpp

--- 060_1.cpp
+++ 060_1.cpp
@@ -10,7 +10,7 @@ int main()
   for (int i = 0; i < N; i++)
   {
     cin >> A.at(i);
-    A.at(i) = N + 1 - A.at(i);
+    A.at(i) = N + 1 - A.at(i); // 単調減少 → 単調増加 を作る問題にする
   }
 
   vector<int> dp(N + 2, INF);
@@ -18,12 +18,14 @@ int main()
 
   for (auto a : A)
   {
+    // 通常の LIS で選べる ｢a を使う｣ ｢何もしない｣ に加え ｢-a を使う｣ も選べる, これで単調増加を作る問題になった
+    
     auto itmi = lower_bound(dp.begin(), dp.end(), -a);
     auto itpl = lower_bound(dp.begin(), dp.end(), a);
 
     int imi = itmi - dp.begin(), ipl = itpl - dp.begin();
 
-    if (dp.at(ipl - 1) != -a) dp.at(ipl) = a;
+    if (dp.at(ipl - 1) != -a) dp.at(ipl) = a; // ただし -2, 2 みたいになるとやばいのでそれだけ除外
     dp.at(imi) = -a;
   }
 
